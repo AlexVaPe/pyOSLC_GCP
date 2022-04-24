265b496b9ff7aaa03adc7cc328e4173e5ea32400
Alejandro Vargas Pérez <alez101010@gmail.com>
2022-04-24 17:46:05 +0200

    http.client

--- oslcapi/api/resources/resourceOSLC.py
+++ oslcapi/api/resources/resourceOSLC.py
@@ -225,7 +225,7 @@ class OSLCAction(Resource):
                     action.add_result('OK')
                     # Generate creation Event
                     event_graph.add((action.uri, RDF.type, Literal(action.action_type)))
-                    conn = http.client.HTTPSConnection(event_endpoint)
+                    conn = http.client.HTTPSConnection('www.tfm-google.duckdns.org/event/payload', 5002)
 
                     headers = {'Content-type': 'application/rdf+xml'}
 
