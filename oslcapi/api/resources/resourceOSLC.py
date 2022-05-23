1b4bce7ec1c20967bd3d3b4ef1aed8bbc1574cfb
Alejandro Vargas Pérez <alez101010@gmail.com>
2022-05-23 19:51:20 +0200

    added api_version on kafka

--- oslcapi/api/resources/resourceOSLC.py
+++ oslcapi/api/resources/resourceOSLC.py
@@ -21,6 +21,7 @@ PROJECT_ID = "weighty-time-341718"
 # Initialize Kafka producer
 my_producer = KafkaProducer(
     bootstrap_servers = ['localhost:29092'],
+    api_version=(0,11,5),
     value_serializer = lambda x: json.dumps(x).encode('utf-8')
     )
 
