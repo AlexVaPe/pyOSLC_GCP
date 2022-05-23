18488d3780a1d2469f8a7374d3a6755c95d58ada
Alejandro Vargas Pérez <alez101010@gmail.com>
2022-05-23 20:00:38 +0200

    kafka corrections

--- oslcapi/api/resources/resourceOSLC.py
+++ oslcapi/api/resources/resourceOSLC.py
@@ -20,7 +20,7 @@ PROJECT_ID = "weighty-time-341718"
 
 # Initialize Kafka producer
 my_producer = KafkaProducer(
-    bootstrap_servers = ['localhost:29092'],
+    bootstrap_servers = ['kafka:29092'],
     api_version=(0,11,5),
     value_serializer = lambda x: json.dumps(x).encode('utf-8')
     )
