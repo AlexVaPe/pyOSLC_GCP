afad9a5bb8163148a5f07b714b6910a90e82a3b3
Alejandro Vargas Pérez <alez101010@gmail.com>
2022-05-28 11:43:19 +0200

    testing fuseki

--- oslcapi/api/helpers/service_events.py
+++ oslcapi/api/helpers/service_events.py
@@ -1,5 +1,4 @@
-import logging
-import os
+import logging, requests
 from rdflib import Namespace, Literal, Graph
 from rdflib.namespace import DCTERMS, RDF, RDFS
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
@@ -14,19 +13,38 @@ OSLC_EVENT = Namespace('http://open-services.net/ns/events#')
 
 # Connect to fuseki triplestore.
 fuseki_store = SPARQLUpdateStore()
+fuseki_endpoint = "fuseki.demos.gsi.upm.es"
+fuseki_dataset = "oslc-gc"
 query_endpoint = 'https://fuseki.demos.gsi.upm.es/oslc-gc/query'
 update_endpoint = 'https://fuseki.demos.gsi.upm.es/oslc-gc/update'
 fuseki_store.open((query_endpoint, update_endpoint))
 
+def sendDataToFuseki(data, host, port ,dataset):
+    url = 'http://{fuseki}:{port}/{dataset}/data'.format(
+        fuseki = host,
+        port = port,
+        dataset = dataset
+    )
+
+    response = requests.put(
+        url,
+        headers = {'Content-Type':'application/ld+json'},
+        data = data
+    )
+
+    return response
 
 def generate_creation_event(resource, store):
     log.warning('Creation event generated')
 
     store.trs.generate_change_event(resource, 'Creation')
     # Generate OSLC Event Resource
-    g = Graph(fuseki_store, identifier=default)
+    #g = Graph(fuseki_store, identifier=default)
+    g = Graph()
     g.add((resource.uri, RDF.type, OSLC_EVENT.Event))
     g.add((resource.uri, DCTERMS.description, Literal('Creation Event')))
+
+    sendDataToFuseki(g, fuseki_endpoint, 80, fuseki_dataset)
 
     return g
 
@@ -57,5 +75,7 @@ def generate_deletion_event(resource, store):
     g = Graph(fuseki_store, identifier=default)
     g.add((resource.uri, RDF.type, OSLC_EVENT.Event))
     g.add((resource.uri, DCTERMS.description, Literal('Deletion Event')))
+
+    sendDataToFuseki(g, fuseki_endpoint, 80, fuseki_dataset)
 
     return g
